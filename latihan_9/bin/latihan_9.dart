71e0bbf2d609f9fd515998b35315167f99b198eb
jjeeeiiiyyaaa <angelina.jessica25@gmail.com>
2023-09-22 10:25:32 +0700

    first commit

---
+++ latihan_9/bin/latihan_9.dart
@@ -0,0 +1,5 @@
+int count = 0;
+do {
+  print('Iterasi ke-$count');
+  count++;
+} while (count < 5);
---
+++ latihan_9/test/latihan_9_test.dart
@@ -0,0 +1,8 @@
+import 'package:latihan_9/latihan_9.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
